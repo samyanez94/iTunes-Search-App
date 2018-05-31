c6c4239043fb97a1f04bf10400ee6921284f60bb
Samuel Yanez <samuelyanez94@gmail.com>
2018-05-30 22:12:38 -0700

    Implement search bar

---
+++ iTunesClient/Controllers/SearchResultsController.swift
@@ -0,0 +1,36 @@
+//
+//  SearchResultsController.swift
+//  iTunesClient
+//
+//  Created by Samuel Yanez on 5/30/18.
+//  Copyright © 2018 Samuel Yanez. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultController))
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+    }
+    
+    @objc func dismissSearchResultController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print(searchController.searchBar.text)
+    }
+    
+    
+}
