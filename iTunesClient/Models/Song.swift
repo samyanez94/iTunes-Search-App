36029be77047abff293c1109df0d280697631689
Samuel Yanez <samuelyanez94@gmail.com>
2018-05-30 20:48:42 -0700

    Add the Song structure and sample data

---
+++ iTunesClient/Models/Song.swift
@@ -0,0 +1,17 @@
+//
+//  Song.swift
+//  iTunesClient
+//
+//  Created by Samuel Yanez on 5/30/18.
+//  Copyright © 2018 Samuel Yanez. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+}
