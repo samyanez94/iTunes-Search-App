2977c36345b33a640ab9ae1cffe1db001db7e4f9
Samuel Yanez <samuelyanez94@gmail.com>
2018-05-30 20:05:54 -0700

    Add Artist, Album, and Genre models

---
+++ iTunesClient/Models/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  iTunesClient
+//
+//  Created by Samuel Yanez on 5/30/18.
+//  Copyright © 2018 Samuel Yanez. All rights reserved.
+//
+
+import Foundation
+
+class Album {
+    
+}
---
+++ iTunesClient/Models/Artist.swift
@@ -0,0 +1,23 @@
+//
+//  Artist.swift
+//  iTunesClient
+//
+//  Created by Samuel Yanez on 5/30/18.
+//  Copyright © 2018 Samuel Yanez. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+}
