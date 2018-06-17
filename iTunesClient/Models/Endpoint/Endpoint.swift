c78da817acf53abbea363744a1150c4d2183f214
Samuel Yanez <samuelyanez94@gmail.com>
2018-06-16 17:24:08 -0700

    Define endpoint protocol

--- iTunesClient/Models/Endpoint/Endpoint.swift
+++ iTunesClient/Models/Endpoint/Endpoint.swift
@@ -8,6 +8,27 @@
 
 import Foundation
 
+protocol Endpoint {
+    var base: String { get }
+    var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+extension Endpoint {
+    var urlComponents: URLComponents {
+        var components = URLComponents(string: base)!
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components
+    }
+    
+    var request: URLRequest {
+        let url = urlComponents.url!
+        return URLRequest(url: url)
+    }
+}
+
 enum Itunes {
     case search(term: String, mediaType: ItunesMedia?)
 }
